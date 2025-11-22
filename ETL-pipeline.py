1c6320916237f7ced045cd42597002c512d465bd
aeMyst <panpetertran123@gmail.com>
2025-11-22 13:51:13 -0700

    supabase ingest complete

--- ETL-pipeline.py
+++ ETL-pipeline.py
@@ -2,9 +2,14 @@ import pyspark.pandas as ps
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
 
+from dotenv import load_dotenv
+import os
+
+os.environ["HADOOP_HOME"] = r"C:\hadoop"
+os.environ["hadoop.home.dir"] = r"C:\hadoop"
 
 # ============================================
-# STEP 1: Spark Session
+# STEP 1: Spark Session + DB Connection Setup
 # ============================================
 
 spark = (
@@ -18,8 +23,28 @@ spark = (
         .config("spark.sql.shuffle.partitions", "4")  # Reduce for local mode
         .config("spark.sql.adaptive.enabled", "true")  # Adaptive query execution
         .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
+
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
         .getOrCreate()
 )
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+jdbc_url = (
+    f"jdbc:postgresql://{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
+    "?sslmode=require"
+    "&prepareThreshold=0"
+    "&reWriteBatchedInserts=true"  # better batch performance
+)
+
+connection_props = {
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "driver": "org.postgresql.Driver"
+}
 
 # ============================================
 # STEP 2: Extract Data From CSV File
@@ -55,8 +80,8 @@ sales_df = sales_df.fillna({
 })
 
 #debug print
-print("Sales Table:")
-print(sales_df.head(4))
+#print("Sales Table:")
+#print(sales_df.head(4))
 
 # ============================================
 # STEP 4: Creator Table
@@ -66,8 +91,8 @@ print(sales_df.head(4))
 creator_df = df[["creator_id", "developer", "publisher"]]
 
 #debug print
-print("Creator Table:")
-print(creator_df.head(4))
+#print("Creator Table:")
+#print(creator_df.head(4))
 
 # ============================================
 # STEP 5: Games Table
@@ -80,5 +105,43 @@ games_df = df[["games_id", "sales_id", "creator_id", "title", "genre", "console"
 games_df["critic_score"] = games_df["critic_score"].fillna(0)
 
 #debug print
-print("Games Table:")
-print(games_df.head(4))
+#print("Games Table:")
+#print(games_df.head(4))
+
+# ============================================
+# STEP 6: Convert to Spark DataFrames
+# ============================================
+
+sales_spark_df   = sales_df.to_spark()
+creator_spark_df = creator_df.to_spark()
+games_spark_df   = games_df.to_spark()
+
+# ============================================
+# STEP 7: Write to PostgreSQL Database
+# ============================================
+
+# sales table
+sales_spark_df.write.jdbc(
+    url=jdbc_url,
+    table="public.Sales",
+    mode="overwrite",          # overwrite for testing purposes
+    properties=connection_props
+)
+
+# creator table
+creator_spark_df.write.jdbc(
+    url=jdbc_url,
+    table="public.Creator",
+    mode="overwrite",   # overwrite for testing purposes
+    properties=connection_props
+)
+
+# games table
+games_spark_df.write.jdbc(
+    url=jdbc_url,
+    table="public.Games",
+    mode="overwrite",   # overwrite for testing purposes
+    properties=connection_props
+)
+
+print("-----Pipeline Write Complete-----")
